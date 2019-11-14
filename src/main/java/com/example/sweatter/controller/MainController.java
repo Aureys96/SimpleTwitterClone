8c27de14b75ec86a743509c44cb33daf50a76840
Kostyan <konstantingolubev8@gmail.com>
2019-11-14 22:01:29 +0300

    added registration & authorization

--- src/main/java/com/example/sweatter/controller/MainController.java
+++ src/main/java/com/example/sweatter/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.sweatter;
+package com.example.sweatter.controller;
 import com.example.sweatter.domain.Message;
 import com.example.sweatter.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,22 +13,21 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController{
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name ="name",required = false,defaultValue = "World") String name, Map<String,Object> model){
+    @GetMapping("/")
+    public String greeting(Map<String,Object> model){
 
-        model.put("name",name);
         return "greeting";
     }
-    @GetMapping
+    @GetMapping("/main")
     public String main(Map <String, Object> model){
        Iterable<Message> messages = messageRepo.findAll();
         model.put("messages", messages);
         return "main";
     }
-    @PostMapping
+    @PostMapping("/main")
     public String add(@RequestParam String text, @RequestParam String tag, Map <String, Object> model){
         Message message = new Message(text,tag);
         messageRepo.save(message);
---
+++ src/main/java/com/example/sweatter/domain/Role.java
@@ -0,0 +1,5 @@
+package com.example.sweatter.domain;
+
+public enum Role {
+    USER;
+}
