eff3d928f72384a6114e9fef128be49fc40f9307
Kostyan <konstantingolubev8@gmail.com>
2019-11-15 20:52:43 +0300

    some minor visual changes

--- src/main/java/com/example/sweatter/config/WebSecurityConfig.java
+++ src/main/java/com/example/sweatter/config/WebSecurityConfig.java
@@ -19,7 +19,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/registration").permitAll()
+                .antMatchers("/","/registration","/test")
+                .permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
--- src/main/java/com/example/sweatter/controller/RegistrationController.java
+++ src/main/java/com/example/sweatter/controller/RegistrationController.java
@@ -3,7 +3,6 @@ package com.example.sweatter.controller;
 import com.example.sweatter.domain.Role;
 import com.example.sweatter.domain.User;
 import com.example.sweatter.repos.UserRepo;
-import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
